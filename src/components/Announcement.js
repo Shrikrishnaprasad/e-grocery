65c70a2e93d582b116f4929bc2b4754ed6f42f06
Shrikrishnaprasad <sgkrishna619@gmail.com>
2021-10-20 08:56:41 +0530

    changed to Css

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,18 +1,23 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+import "./Announcement.css";
 
-const Container = styled.div`
-  height: 30px;
-  background-color: purple;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 14px;
-  font-weight: 500;
-`;
+// const Container = styled.div`
+//   height: 30px;
+//   background-color: purple;
+//   color: white;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   font-size: 14px;
+//   font-weight: 500;
+// `;
 
 const Announcement = () => {
-  return <Container>Super Deal! Free Shipping on Orders Over ₹ 500</Container>;
+  return (
+    <div className="announcement-Container">
+      Super Deal! Free Shipping on Orders Over ₹ 500
+    </div>
+  );
 };
 
 export default Announcement;
